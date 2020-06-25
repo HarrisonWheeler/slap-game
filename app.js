0ab4bdb41ff800d0e30f006f72ebeccb8d261ecf
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-06-25 14:31:39 -0600

    update

--- app.js
+++ app.js
@@ -13,41 +13,41 @@ let buttons = {
 
 
 // ANCHOR Define the slap, kick, and punch functions
-function slap(){
+function slap() {
   health--;
- console.log(health);
- 
- //  drawHealth()
+  console.log(health);
+
+  //  drawHealth()
 }
 
-function kick(){
-  health-5;
- console.log(health);
- 
- //  drawHealth()
+function kick() {
+  health - 5;
+  console.log(health);
+
+  //  drawHealth()
 }
 
-function punch(){
-  health-10;
- console.log(health);
- 
- //  drawHealth()
+function punch() {
+  health - 10;
+  console.log(health);
+
+  //  drawHealth()
 }
 
 
 
 
-function drawHealth(){
+function drawHealth() {
   // after a button press, send "health" number to the header where the health output box is
-  
+
 }
 
 // ANCHOR Dynamically add the buttons to the page. This allows buttons to be added later.
-function drawButtons(){
+function drawButtons() {
   let template = ""
   for (let key in buttons) {
-      let item = buttons[key]
-      template += /*html*/`
+    let item = buttons[key]
+    template += /*html*/`
       html here
       `
   }
